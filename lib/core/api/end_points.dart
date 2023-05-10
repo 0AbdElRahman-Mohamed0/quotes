995c2d2a1ab20f82a01e30752e718d574f11212f
AbdElRahman <abdelrahman-mohamed98@outlook.com>
2023-05-10 04:59:02 +0300

    implementation of remote data source

---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,4 @@
+class EndPoints {
+  static const String baseUrl = 'http://api.api-ninjas.com/v1/';
+  static const String randomQuote = '${baseUrl}quotes';
+}
--- lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
+++ lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
@@ -1,5 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:quote_learn/core/api/end_points.dart';
+import 'package:quote_learn/core/error/exceptions.dart';
 import 'package:quote_learn/features/random_quote/data/models/quote_model.dart';
 
 abstract class RandomQuoteRemoteDataSource {
   Future<QuoteModel> getRandomQuote();
+}
+
+class RandomQuoteRemoteDataSourceImplementation
+    implements RandomQuoteRemoteDataSource {
+  http.Client client;
+  RandomQuoteRemoteDataSourceImplementation({required this.client});
+
+  @override
+  Future<QuoteModel> getRandomQuote() async {
+    final randomQuote = Uri.parse(EndPoints.randomQuote);
+    final response = await client.get(randomQuote, headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': 'jOZFNr1V9zpvFwJH8MYNMQ==DLo6efbYA8niNmb4',
+    });
+    if (response.statusCode == 200) {
+      return QuoteModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
